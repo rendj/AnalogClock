22b5113ba245c2fdcc1536e5322351402827e082
Andrew Popov <apopov@brightgrove.com>
2019-07-13 17:14:48 +0300

    Initial commit

---
+++ AnalogClock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnalogClock
+//
+//  Created by Andrii on 7/13/19.
+//  Copyright © 2019 Andrii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
