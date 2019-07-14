95e587fe591da27f39547992c84c48e317f04ea5
Andrew Popov <apopov@brightgrove.com>
2019-07-14 13:44:06 +0300

    Added DateUtils

---
+++ AnalogClock/DateUtils.swift
@@ -0,0 +1,37 @@
+//
+//  DateUtils.swift
+//  AnalogClock
+//
+//  Created by Andrii on 7/14/19.
+//  Copyright © 2019 Andrii. All rights reserved.
+//
+
+import Foundation
+
+enum DateComponetType {
+    case second
+    case minute
+    case hour
+    
+    var dateFormat: String {
+        switch self {
+        case .second:
+            return "ss"
+        case .minute:
+            return "mm"
+        case .hour:
+            return "hh"
+        }
+    }
+}
+
+class DateUtils {
+
+    func currentValue(for type: DateComponetType) -> Int {
+        let formatter = DateFormatter()
+        formatter.dateFormat = type.dateFormat
+        let valueString = formatter.string(from: Date())
+        precondition(Int(valueString) != nil)
+        return Int(valueString)!
+    }
+}
